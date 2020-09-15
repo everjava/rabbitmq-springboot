4486cdd8a0c4c61e3b7225e3e2c0dffeb7be909a
everjava <everjava@yahoo.com.br>
2020-09-15 10:04:19 -0300

    First commit

---
+++ src/main/java/br/com/ever/Person.java
@@ -0,0 +1,33 @@
+package br.com.ever;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+	private static final long serialVersionUID = 2191264193154014982L;
+	private Long id;
+	private String name;
+
+	public Person(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/main/java/br/com/ever/RabbitMqApplication.java
@@ -0,0 +1,13 @@
+package br.com.ever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication 
+public class RabbitMqApplication {
+
+	public static void main(String args[]) {
+		SpringApplication.run(RabbitMqApplication.class, args);
+	}
+	
+}
